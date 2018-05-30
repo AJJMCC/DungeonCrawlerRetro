f6de813906104aeab1c33bb7bbeea2615313dc7c
Unknown <1004436@student.sae.edu.au>
2018-05-30 15:06:49 +1000

    Real initial commit
    Has a scene for the map tool and a scene for the character tool

---
+++ dungeon crawler Demo/Assets/Scripts/MapMakerMaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMakerMaster : MonoBehaviour {
+
+    private MapGenScript MapMakerSlave;
+
+    public Transform[] mapObjectAnchorss;
+
+    public Texture2D[] MapTextures;
+
+    private int Level = 0;
+    private int CurrentlevelsReady = 0;
+
+    // Use this for initialization
+    void Start () {
+        MapMakerSlave = this.GetComponent<MapGenScript>();
+        CurrentlevelsReady = MapTextures.Length;
+        Debug.Log(CurrentlevelsReady);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (Level < CurrentlevelsReady)
+            {
+                CommandSlave();
+                Debug.Log("made map");
+            }
+            else
+                Debug.LogError("you dont have a map to make so piss off");
+          
+        }
+	}
+
+    void CommandSlave()
+    {
+
+        MapMakerSlave.RecieveCommand(mapObjectAnchorss[Level], MapTextures[Level], Level);
+        Level++;
+    }
+}
---
+++ dungeon crawler Demo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public static PlayerMovement Instance;
+
+    [Tooltip("the transofrm to turn")]
+    public Transform PlayerTrans;
+    [Tooltip("the rigidbody to move about")]
+    public Rigidbody PB;
+   
+
+    [Tooltip("how fast we move")]
+    public float BaseMoveSpeed;
+
+   
+
+
+    [Tooltip("how fast we turn")]
+    public float BaseTurnSpeed;
+    private float YROT;
+
+    [Tooltip("This is so our player controller can disable movement at need")]
+    public bool CanPlayerMove = true;
+    [Tooltip("This is so our player controller can disable rotation at need")]
+    public bool CanPlayerTurn = true;
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	//this setup uses Axis controls instead of keys so later we can support multiple control schemes and it is super simple
+	//to change the sluggishness of movement and so on change the horizontal axis input controls 
+    //to do that go to: Edit > Project Settings > Input
+    //change "Vertical" for movement
+    //change "Horizontal" for rotation
+	void Update ()
+    {
+        if (CanPlayerMove)
+        MovementChecks();
+
+        if (CanPlayerTurn)
+        TurnChecks();
+        
+    }
+
+    void MovementChecks()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            PB.velocity = PlayerTrans.forward * (BaseMoveSpeed * Input.GetAxis("Vertical")) ;
+        }
+    }
+   
+    void TurnChecks()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            YROT += (BaseMoveSpeed * Input.GetAxis("Horizontal"));
+            PlayerTrans.rotation = Quaternion.Euler(Vector3.up * YROT);
+        }
+    }
+   
+}
+
